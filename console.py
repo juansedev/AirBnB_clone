e990d4c81a510af03fc1d4f96629939da5018bf3
Emma Navarro <elnavarro55@gmail.com>
2020-06-28 02:07:06 +0000

    Update do_update

--- console.py
+++ console.py
@@ -4,6 +4,7 @@
 
 import cmd
 from models import storage
+from models.engine.file_storage import FileStorage
 from shlex import split
 from models.base_model import BaseModel
 
@@ -69,9 +70,56 @@ class HBNBCommand(cmd.Cmd):
             storage.reload()
             for key, instance in storage.all().items():
                 if instance.__class__.__name__ == args[0] and instance.id == args[1]:
-                    del(instance)
+                    del(storage.all()[key])
                     storage.save()
                     return
+            print("** no instance found **")
+
+    def do_all(self, arg):
+        """ Prints str representation of instances """
+        args = split(arg)
+        if args == []:
+            pass
+        elif args[0] not in classes:
+            print("** class doesn't exist **")
+        else:
+            storage.reload()
+            inst_list = []
+            for key, instance in storage.all().items():
+                if instance.__class__.__name__ == args[0]:
+                    inst_list.append(instance.__str__())
+            print(inst_list)
+
+    def do_update(self, arg):
+        """ Update an instance baed on the class name
+        and id by adding or updating attribute
+        """
+        args = split(arg)
+        inst_list = storage.all()
+        if args == []:
+            print("** class name missing **")
+        elif args[0] not in classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        else:
+            storage.reload()
+            for key, instance in inst_list.items():
+                if instance.__class__.__name__ == args[0] and instance.id == args[1]:
+                    if  len(args) == 2:
+                        print("** attribute name missing **")
+                        return
+                    elif len(args) == 3:
+                        print("** value missing **")
+                        return
+                    else:
+                        key_object = args[0] + "." + args[1]
+                        print(key_object)
+                        object = inst_list[key_object]
+                        print("-> {}".format(inst_list[key_object]))
+                        setattr(object, args[2], args[3])
+                        object.save()
+                        return
             print("** no instance found **")
 
 if __name__ == "__main__":

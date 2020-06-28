57c8ee74f491535ff45a6e45fed15e015ef5d55b
Emma Navarro <elnavarro55@gmail.com>
2020-06-28 17:38:10 +0000

    Update Console.py

--- console.py
+++ console.py
@@ -8,9 +8,15 @@ from models.engine.file_storage import FileStorage
 from shlex import split
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 
-classes = {'BaseModel': BaseModel, 'User': User}
+classes = {'BaseModel': BaseModel, 'User': User, 'State': State, 'City': City,
+           'Place': Place, 'Amenity': Amenity, 'Review': Review}
 
 
 class HBNBCommand(cmd.Cmd):
